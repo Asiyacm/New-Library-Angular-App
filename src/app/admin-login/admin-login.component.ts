6387db31cb4578705d4d006ba3f09a6af3480218
Asiyacm <asiyacmuhammed@gmail.com>
2022-12-09 01:12:44 +0530

    adminLogin commit

---
+++ src/app/admin-login/admin-login.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-login',
+  templateUrl: './admin-login.component.html',
+  styleUrls: ['./admin-login.component.css']
+})
+export class AdminLoginComponent {
+
+  username=""
+  password=""
+
+  adminLogin=()=>
+  {
+    let admin:any=
+    {"username":this.username,"password":this.password}
+       console.log(admin)
+  
+
+}
+}
